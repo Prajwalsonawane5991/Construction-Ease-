2c8e28b325031adef54332733e45d8fbe3f9409e
Tushar-exe <patle.tushar2000@gmail.com>
2024-02-12 01:33:26 +0530

    emailservice+OTP

---
+++ src/main/java/com/example/demo/dto/ResisterDto.java
@@ -0,0 +1,27 @@
+package com.example.demo.dto;
+
+
+public class ResisterDto {
+	private String name;
+	   private String email;
+	   private String Password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+}
